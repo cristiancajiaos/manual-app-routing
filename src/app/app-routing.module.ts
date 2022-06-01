581be27691865b219f50ebcea95e822250dad35d
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-01 10:25:18 -0400

    Creados componentes de prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { BazComponent } from './baz/baz.component';
+import { BarComponent } from './bar/bar.component';
+import { FooComponent } from './foo/foo.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'foo', component: FooComponent },
+  { path: 'bar', component: BarComponent },
+  { path: 'baz', component: BazComponent },
+  { path: '**', redirectTo: '/foo', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { BazComponent } from './baz/baz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    FooComponent,
+    BarComponent,
+    BazComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
